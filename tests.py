f7cad387d46a015ba1aed75d339bf53f3fa651c3
Geert Jan Talens <talensgj@gmail.com>
2024-08-28 15:02:58 +0100

    Re-factored tests.

--- tests.py
+++ tests.py
@@ -16,116 +16,6 @@ RNG = np.random.default_rng(5627323756)
 SECINDAY = 24*3600
 
 
-def bin_lightcurve(lc_data, bin_size=12):
-    """ Re-bin a plato lightcurve based on a cadence.
-    """
-
-    bin_idx = np.arange(len(lc_data)) // bin_size
-
-    weights = 1/lc_data['flux_err']**2
-
-    val0 = np.bincount(bin_idx)
-    val1 = np.bincount(bin_idx, weights=lc_data['time'])
-    val2 = np.bincount(bin_idx, weights=weights*lc_data['flux'])
-    val3 = np.bincount(bin_idx, weights=weights)
-
-    time = val1/val0
-    flux = val2/val3
-    flux_err = np.sqrt(1/val3)
-
-    # plt.plot(lc_data['time'][::10], lc_data['flux'][::10], '.')
-    # plt.errorbar(time, flux, yerr=flux_err, ls='none')
-    # plt.show()
-    # plt.close()
-
-    return time, flux, flux_err
-
-
-def real_test():
-
-    lc_id = 99
-    lc_dir = '/home/talens/Research/PLATO/plato_simulations/WP111000_simulations/simulations_Breton/complete_lightcurve_habitable'
-
-    # Read the lightcurve.
-    lc_file = os.path.join(lc_dir, f'{lc_id * 100:010d}.ftr')
-    lc_data = pd.read_feather(lc_file)
-
-    # Bin to 5-min resolution.
-    time, flux, flux_err = bin_lightcurve(lc_data)
-
-    # Use a simple biweight detrending.
-    flux_biweight, trend_biweight = wotan.flatten(time,
-                                                  flux,
-                                                  method='biweight',
-                                                  window_length=2.1,
-                                                  edge_cutoff=0.5,
-                                                  break_tolerance=0.5,
-                                                  cval=5,
-                                                  return_trend=True)
-    flux_biweight_err = flux_err / trend_biweight
-
-    # Remove bad points.
-    mask = np.isfinite(trend_biweight)
-    tls = transitlstsq.transitleastsquares(time[mask], flux_biweight[mask], flux_biweight_err[mask], verbose=True)
-    tls_result = tls.power(period_min=135,
-                           period_max=145,
-                           n_transits_min=1,
-                           use_threads=8)
-
-    min_duration = 0.10996734401247639
-    max_duration = 1.4531496279883123
-    template_time, template_models, _ = search.template_grid(140.,
-                                                             min_duration,
-                                                             max_duration,
-                                                             2 / (24 * 12),
-                                                             impact_params=(0, 0.95))
-
-    print(template_models.shape)
-    plt.imshow(template_models)
-    plt.show()
-
-    flux_biweight_err = flux_biweight_err/np.nanmean(flux_biweight_err)  # Same as TLS
-    result = search.template_lstsq(time[mask],
-                                   flux_biweight[mask],
-                                   flux_biweight_err[mask],
-                                   tls_result.periods,
-                                   min_duration,
-                                   max_duration,
-                                   template_time,
-                                   template_models)
-
-    my_periods = result[0]
-    my_chi2 = result[1]
-    phase = result[5]
-    model = result[6]
-    sort = np.argsort(phase)
-    phase = phase[sort]
-    flux = flux_biweight[mask][sort]
-    model = model[sort]
-
-    plt.figure(figsize=(8, 5))
-
-    plt.subplot(211)
-    plt.plot(tls_result.periods, tls_result.chi2)
-    plt.plot(my_periods, my_chi2)
-
-    plt.xlabel('Period [days]')
-    plt.ylabel('Chi2')
-
-    plt.subplot(212)
-    plt.plot(phase, flux, 'k.')
-    plt.plot(phase, model, lw=3)
-
-    plt.xlim(-max_duration/140.+0.5, max_duration/140.+0.5)
-
-    plt.xlabel('Phase')
-    plt.ylabel('Flux')
-
-    plt.tight_layout()
-    plt.savefig('template_test.png', dpi=300)
-    plt.show()
-
-
 def make_test_lightcurve(length: float,
                          period: float,
                          depth: float,
@@ -205,7 +95,7 @@ def make_single_template(period: float,
     return template_edges, template_model
 
 
-def test_steps():
+def test_bin_sizes():
 
     true_period = 3.4849
     true_duration = 3.4/24
@@ -281,6 +171,120 @@ def test_steps():
     fig.tight_layout()
     plt.show()
 
+    return
+
+
+# def bin_lightcurve(lc_data, bin_size=12):
+#     """ Re-bin a plato lightcurve based on a cadence.
+#     """
+#
+#     bin_idx = np.arange(len(lc_data)) // bin_size
+#
+#     weights = 1/lc_data['flux_err']**2
+#
+#     val0 = np.bincount(bin_idx)
+#     val1 = np.bincount(bin_idx, weights=lc_data['time'])
+#     val2 = np.bincount(bin_idx, weights=weights*lc_data['flux'])
+#     val3 = np.bincount(bin_idx, weights=weights)
+#
+#     time = val1/val0
+#     flux = val2/val3
+#     flux_err = np.sqrt(1/val3)
+#
+#     # plt.plot(lc_data['time'][::10], lc_data['flux'][::10], '.')
+#     # plt.errorbar(time, flux, yerr=flux_err, ls='none')
+#     # plt.show()
+#     # plt.close()
+#
+#     return time, flux, flux_err
+#
+#
+# def real_data_test():
+#
+#     lc_id = 99
+#     lc_dir = '/home/talens/Research/PLATO/plato_simulations/WP111000_simulations/simulations_Breton/complete_lightcurve_habitable'
+#
+#     # Read the lightcurve.
+#     lc_file = os.path.join(lc_dir, f'{lc_id * 100:010d}.ftr')
+#     lc_data = pd.read_feather(lc_file)
+#
+#     # Bin to 5-min resolution.
+#     time, flux, flux_err = bin_lightcurve(lc_data)
+#
+#     # Use a simple biweight detrending.
+#     flux_biweight, trend_biweight = wotan.flatten(time,
+#                                                   flux,
+#                                                   method='biweight',
+#                                                   window_length=2.1,
+#                                                   edge_cutoff=0.5,
+#                                                   break_tolerance=0.5,
+#                                                   cval=5,
+#                                                   return_trend=True)
+#     flux_biweight_err = flux_err / trend_biweight
+#
+#     # Remove bad points.
+#     mask = np.isfinite(trend_biweight)
+#     tls = transitlstsq.transitleastsquares(time[mask], flux_biweight[mask], flux_biweight_err[mask], verbose=True)
+#     tls_result = tls.power(period_min=135,
+#                            period_max=145,
+#                            n_transits_min=1,
+#                            use_threads=8)
+#
+#     min_duration = 0.10996734401247639
+#     max_duration = 1.4531496279883123
+#     template_time, template_models, _ = search.template_grid(140.,
+#                                                              min_duration,
+#                                                              max_duration,
+#                                                              2 / (24 * 12),
+#                                                              impact_params=(0, 0.95))
+#
+#     print(template_models.shape)
+#     plt.imshow(template_models)
+#     plt.show()
+#
+#     flux_biweight_err = flux_biweight_err/np.nanmean(flux_biweight_err)  # Same as TLS
+#     result = search.template_lstsq(time[mask],
+#                                    flux_biweight[mask],
+#                                    flux_biweight_err[mask],
+#                                    tls_result.periods,
+#                                    min_duration,
+#                                    max_duration,
+#                                    template_time,
+#                                    template_models)
+#
+#     my_periods = result[0]
+#     my_chi2 = result[1]
+#     phase = result[5]
+#     model = result[6]
+#     sort = np.argsort(phase)
+#     phase = phase[sort]
+#     flux = flux_biweight[mask][sort]
+#     model = model[sort]
+#
+#     plt.figure(figsize=(8, 5))
+#
+#     plt.subplot(211)
+#     plt.plot(tls_result.periods, tls_result.chi2)
+#     plt.plot(my_periods, my_chi2)
+#
+#     plt.xlabel('Period [days]')
+#     plt.ylabel('Chi2')
+#
+#     plt.subplot(212)
+#     plt.plot(phase, flux, 'k.')
+#     plt.plot(phase, model, lw=3)
+#
+#     plt.xlim(-max_duration/140.+0.5, max_duration/140.+0.5)
+#
+#     plt.xlabel('Phase')
+#     plt.ylabel('Flux')
+#
+#     plt.tight_layout()
+#     plt.savefig('template_test.png', dpi=300)
+#     plt.show()
+#
+#     return
+
 
 if __name__ == '__main__':
-    test_steps()
+    test_bin_sizes()
