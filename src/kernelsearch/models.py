0bce957dc8b561528b48dad6969bfac4d7cc12dc
Geert Jan Talens <talensgj@gmail.com>
2026-02-19 13:44:20 +0000

    Remove unused functions from models.py

--- src/kernelsearch/models.py
+++ src/kernelsearch/models.py
@@ -47,18 +47,6 @@ def axis2duration(a, per, p, b, ecc, w):
     return transit_duration
 
 
-def axis2full(a, per, p, b, ecc, w):
-    """"""
-
-    sin_sq = ((1 - p) ** 2 - b ** 2) / (a ** 2 - b ** 2)
-    transit_full = per / np.pi * np.arcsin(np.sqrt(sin_sq))
-
-    # Eccentricity correction (for transits).
-    transit_full = transit_full * np.sqrt(1 - ecc ** 2) / (1 + ecc * np.sin(w * DEG2RAD))
-
-    return transit_full
-
-
 def duration2axis(transit_duration, per, p, b, ecc, w):
     """ Convert the scaled semi-major axis to transit duration.
     """
@@ -99,78 +87,6 @@ def density2axis(rho, per):
     a = a.decompose()
 
     return a.value
-
-
-def transit_masks(time: np.ndarray,
-                  transit_params: dict,
-                  window: float = 3.
-                  ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
-    """ Compute various useful masks and number from orbital parameters.
-    """
-
-    # Input parameters.
-    t0 = transit_params['T_0']
-    per = transit_params['P']
-    rp = transit_params['R_p/R_s']
-    a = transit_params['a/R_s']
-    b = transit_params['b']
-    ecc = transit_params['ecc']
-    w = transit_params['w']
-
-    transit_duration = axis2duration(a, per, rp, b, ecc, w)
-
-    phase = (time - t0) / per
-    norbit = np.round(phase).astype('int')
-
-    norbit_unique = np.unique(norbit)
-    midpoint = t0 + norbit_unique * per
-
-    phase = np.mod(phase - 0.5, 1) - 0.5
-    intransit = np.abs(phase) <= 0.5 * transit_duration / per
-    baseline = (np.abs(phase) <= 0.5 * window * transit_duration / per) & ~intransit
-
-    min_orbit = np.amin(norbit_unique)
-    norbit = norbit - min_orbit
-    norbit_unique = norbit_unique - min_orbit
-
-    return norbit, intransit, baseline, norbit_unique, midpoint
-
-
-def latlon2xy(lat, lon, lat0, lon0):
-    """ Convert latitude and longitude to x, y coordinates using an orthographic
-        projection.
-    """
-
-    # Convert degrees to radians.
-    lat, lon = lat * DEG2RAD, lon * DEG2RAD
-    lat0, lon0 = lat0 * DEG2RAD, lon0 * DEG2RAD
-
-    xvec = np.cos(lat) * np.sin(lon - lon0)
-    yvec = np.cos(lat0) * np.sin(lat) - np.sin(lat0) * np.cos(lat) * np.cos(lon - lon0)
-    cos_c = np.sin(lat0) * np.sin(lat) + np.cos(lat0) * np.cos(lat) * np.cos(lon - lon0)
-    visible = cos_c >= 0
-
-    return xvec, yvec, visible
-
-
-def xy2latlon(x, y, lat0, lon0):
-    """ Convert x, y coordinates to latitude and longitude using an orthographic
-        projection.
-    """
-
-    lat0, lon0 = lat0 * DEG2RAD, lon0 * DEG2RAD
-
-    rho = np.sqrt(x ** 2 + y ** 2)
-    c = np.arcsin(rho)
-
-    tmp1 = np.cos(c) * np.sin(lat0) + y * np.cos(lat0)
-    lat = np.arcsin(tmp1)
-    tmp2 = rho * np.cos(c) * np.cos(lat0) - y * np.sin(c) * np.sin(lat0)
-    lon = lon0 + np.arctan2(x * np.sin(c), tmp2)
-
-    lat, lon = lat * RAD2DEG, lon * RAD2DEG
-
-    return lat, lon
 
 
 def analytic_transit_model(time: np.ndarray,
@@ -281,49 +197,3 @@ def analytic_transit_model(time: np.ndarray,
         yp = xi * np.sin(Omega) + yi * np.cos(Omega)
 
     return flux, nu, xp, yp, params, model.fac
-
-
-def make_test_lightcurve(length: float,
-                         period: float,
-                         depth: float,
-                         duration: float,
-                         noise_ppm: float = 1000.,
-                         ld_type: str = 'linear',
-                         ld_pars: tuple = (0.6,),
-                         exp_time: float = 0.,
-                         supersampling: int = 1):
-
-    # Compute the scaled semi-major axis.
-    axis = duration2axis(duration,
-                         period,
-                         np.sqrt(depth),
-                         0.,
-                         0.,
-                         90.)
-
-    # A simple transit model.
-    transit_params = dict()
-    transit_params['T_0'] = period*RNG.random()
-    transit_params['P'] = period
-    transit_params['R_p/R_s'] = np.sqrt(depth)
-    transit_params['a/R_s'] = axis
-    transit_params['b'] = 0.
-    transit_params['ecc'] = 0.
-    transit_params['w'] = 90.
-    transit_params['Omega'] = 0.
-
-    npoints = np.ceil(length / exp_time).astype('int')
-    time = np.arange(npoints) * exp_time
-
-    result = analytic_transit_model(time,
-                                    transit_params,
-                                    ld_type,
-                                    ld_pars,
-                                    max_err=1,
-                                    exp_time=exp_time,
-                                    supersample_factor=supersampling)
-
-    flux = result[0] + RNG.normal(size=npoints)*noise_ppm/1e6
-    flux_err = np.ones_like(flux)*noise_ppm/1e6
-
-    return time, flux, flux_err, result[0]
