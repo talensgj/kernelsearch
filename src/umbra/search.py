bc61812487dd39398fdb4f6d145ee9038911bad1
Geert Jan Talens <talensgj@gmail.com>
2026-03-13 13:40:45 +0000

    Rename package to umbra.

--- src/umbra/search.py
+++ src/umbra/search.py
@@ -6,7 +6,7 @@ import numpy as np
 from scipy import signal
 import multiprocessing as mp
 
-from kernelsearch import models
+from . import models
 from transitleastsquares import grid, tls_constants
 
 import matplotlib.pyplot as plt
